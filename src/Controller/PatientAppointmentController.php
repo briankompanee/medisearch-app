4d7d7247f184f677627dd22e7cd3c963fb1e03a9
Brian Brown <brian@thekompanee.com>
2024-05-20 14:54:56 -0500

    Add namespaces for PatientAppointment Controller

--- src/Controller/PatientAppointmentController.php
+++ src/Controller/PatientAppointmentController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Appointment;
+use App\Entity\Patient;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
