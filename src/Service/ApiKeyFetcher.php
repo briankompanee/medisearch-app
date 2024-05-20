3002e14b57d4615d2ef8e1b38638743f76b1e5db
Brian Brown <brian@thekompanee.com>
2024-05-20 13:18:38 -0500

    Create ApiKeyFetcher Service
    create this service to protect the API key

---
+++ src/Service/ApiKeyFetcher.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Service;
+
+class ApiKeyFetcher
+{
+    private string $apiKey;
+
+    public function __construct(string $apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    public function getApiKey(): string
+    {
+        return $this->apiKey;
+    }
+}
