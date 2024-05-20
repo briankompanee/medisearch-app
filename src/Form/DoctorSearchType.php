20321b5cde0a70caa218be45b743110fe8cef578
Brian Brown <brian@thekompanee.com>
2024-05-20 16:24:28 -0500

    Create DoctorSearchType.php
    create Doctor search Class for Form in Symfony to work with twig view

---
+++ src/Form/DoctorSearchType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class DoctorSearchType extends AbstractType
+{
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+    $builder
+      ->add('doctor_name', TextType::class, [
+          'label' => 'Doctor Name',
+          'required' => true,
+      ]);
+  }
+}
