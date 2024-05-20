05d50d1f8a2be9dce6b78329f050d5850dd67ab3
Brian Brown <brian@thekompanee.com>
2024-05-20 14:22:59 -0500

    Update DoctorAvailabilityController.php
    Add support for Mockaroo Service and Entity Manager Interface

--- src/Controller/DoctorAvailabilityController.php
+++ src/Controller/DoctorAvailabilityController.php
@@ -2,12 +2,23 @@
 
 namespace App\Controller;
 
+use App\Service\MockarooService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DoctorAvailabilityController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+    private MockarooService $mockarooService;
+
+    public function __construct(EntityManagerInterface $entityManager, MockarooService $mockarooService)
+    {
+        $this->entityManager = $entityManager;
+        $this->mockarooService = $mockarooService;
+    }
+
     #[Route('/doctor/availability', name: 'app_doctor_availability')]
     public function index(): Response
     {
