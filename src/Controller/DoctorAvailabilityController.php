6c635e55a894916415c2b04536bf11ac090ab1ce
Brian Brown <brian@thekompanee.com>
2024-05-20 14:48:45 -0500

    Add required namespaces for controller

--- src/Controller/DoctorAvailabilityController.php
+++ src/Controller/DoctorAvailabilityController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Doctor;
+use App\Form\DoctorSearchType;
 use App\Service\MockarooService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
